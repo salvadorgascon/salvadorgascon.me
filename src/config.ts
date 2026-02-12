f10806746adf720cad936aaf39dafc3402ab05ef
Salvador Gascon <salvadorgascon@protonmail.com>
2026-02-12 19:27:45 +0100

    Fix light and dark mode

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE = {
   desc: "My personal blog.",
   title: "Salvador Gascon Blog",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerIndex: 4,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
